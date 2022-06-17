bc1da2fab73da541923d4cac57b0585f86fb55c4
DESKTOP-16HEUQ4\ez220404 <hg5112@naver.com>
2022-06-17 14:27:20 +0900

    테이블 생성 초안

---
+++ bg.sql
@@ -0,0 +1,14 @@
+create table component_stock1 (
+name varchar2(20),
+KOR number,
+ENG number,
+MATH number,
+SCI number,
+TOTAL number,
+AVG number,
+GRADE varchar2(20)
+);
+
+SELECT * FROM component_stock1;
+
+drop table component_stock1;
